0fcb4dbf4a4b2efb7e555d978062ff44b7685ecc
KanakPendse14 <2021.kanak.pendse@ves.ac.in>
2023-07-07 08:59:09 +0530

    commit1

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -24,7 +24,7 @@ const HeroSection = () => {
      
       <div className='right'>
         <p className="hero-section-p">
-I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+I'm a paragraph. Click here to add your own text and edit me. It's easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
 <div className="button-div"><a href="/about"><button className='hero-section-button'>Read More</button></a></div>
       </div>
       <div className='left'>
