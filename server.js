a2617261c2165e85d8bbf36e145c29921fe44036
Maverick <manasmahajan8479@gmail.com>
2023-07-17 23:58:52 +0530

    product and category api

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryroutes.js';
+import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
+
 //configure env
 dotenv.config();
 
@@ -20,6 +23,9 @@ app.use(morgan('dev'));
 
 //routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/product', productRoutes);
+
 //rest api
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to ecommerece app</h1>');
